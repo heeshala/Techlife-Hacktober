418920eb4a22e3745719f5626163ced7a951553c
Heeshala Nadeeshan <50227550+heeshala@users.noreply.github.com>
2020-10-01 09:12:58 +0530

    Create Program.c

---
+++ Program.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float name;
+    printf("Welcome to Hacktoberfest 2020\n");
+    printf("Enter your name : ");//Get User name
+    scanf("%d",&yourname);
+
+
+    return 0;
+}
