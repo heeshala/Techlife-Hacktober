56413761ccd318cc81285adb7bae1cacbbccf2d1
Kasun Madhusanka <44221598+Kasunmadhusanka1996@users.noreply.github.com>
2020-10-01 22:02:38 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -5,7 +5,7 @@ int main()
     char name[20];
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
-    scanf("%s",&name);
+    scanf("%s",&name1);
 
 
     return 0;
