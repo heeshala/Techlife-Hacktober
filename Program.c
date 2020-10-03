1f3375276a882eb2a7710caf8528126aac901bc3
Kavidu Madusha <56311015+kavimadusha@users.noreply.github.com>
2020-10-03 21:27:48 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    float name;
+    char name;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
-    scanf("%d",&yourname);
+    scanf("%s",&name);
 
 
     return 0;
