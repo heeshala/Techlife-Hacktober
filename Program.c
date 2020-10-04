654ea76314f30e176f75af875762392819647a6f
Dinith Gamage <56905579+dinithg@users.noreply.github.com>
2020-10-04 10:01:01 +0530

    Added a new variable and output

--- Program.c
+++ Program.c
@@ -3,9 +3,13 @@
 int main()
 {
     char name[20];
+    int age;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
     scanf("%s",&name);
+    printf("Enter your age: ");//Get User's age
+    scanf("%d",&age);
+    
 
 
     return 0;
