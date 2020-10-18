d8cae25a8550def1f9d8c5379cfd524ad9c7a20c
Sunera-2216 <sunerasu2216@gmail.com>
2020-10-19 00:30:16 +0530

    Fixed errors and modified program

--- Program.c
+++ Program.c
@@ -2,11 +2,13 @@
 
 int main()
 {
-    float name;
-    printf("Welcome to Hacktoberfest 2020\n");
+    char name[50];
+    
     printf("Enter your name : ");//Get User name
-    scanf("%d",&yourname);//User inputs the name
+    scanf("%s",name);//User inputs the name
 
+    printf("Hi %s\nWelcome to Hacktoberfest 2020\n", name);
 
     return 0;
 }
+ 
