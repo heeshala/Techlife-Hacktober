2e8c3bd5abe2e8f0f6a1ded5f32e9bb88bc80e56
kanimesha99 <90630567+kanimesha99@users.noreply.github.com>
2021-10-02 07:49:31 -0700

    corrected program

--- Program.c
+++ Program.c
@@ -6,7 +6,7 @@ int main()
     printf("Welcome to Hacktoberfest 2021\n");
     printf("Enter your name : \n");   //Get User name
     scanf("%[^\n]s",&name);  //User inputs the name
-	printf("\nYour name is = %s",name);
+    printf("\nYour name is = %s",name);
     getch();
     return 0;
 }
