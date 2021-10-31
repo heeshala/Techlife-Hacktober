683c1b79bf70fabe52daac83804939318ee7cf76
saurabh3235 <72140254+saurabh3235@users.noreply.github.com>
2021-10-31 20:46:44 +0530

    correction of your program

--- Program.c
+++ Program.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-    float name;
+    char name;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
     scanf("%d",&yourname);//User inputs the name
-
+    printf("%d",yourname);
 
     return 0;
 }
