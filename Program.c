36d50e0935d89774be74ee192f89669e9f13c9bf
Dhanendra Kumar Sonber <dhanendrakumarsonber@gmail.com>
2021-10-31 20:49:56 +0530

    Correcting the program

--- Program.c
+++ Program.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-    float name;
-    printf("Welcome to Hacktoberfest 2020\n");
-    printf("Enter your name : ");//Get User name
-    scanf("%d",&yourname);//User inputs the name
+    char name;
+    printf("Welcome to Hacktoberfest 2021\n");
+    printf("Enter your name:");//Get User name
+    scanf("%s",&name);//User inputs the name
+    printf("Your name is : %s",name);//name will be printed
 
 
     return 0;
