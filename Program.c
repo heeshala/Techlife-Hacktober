732a1a2bc91c40d385958963bd1e9dab5a66bdde
saurabh3235 <72140254+saurabh3235@users.noreply.github.com>
2021-10-31 20:49:25 +0530

    correction to program

--- Program.c
+++ Program.c
@@ -5,8 +5,8 @@ int main()
     char name;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
-    scanf("%d",&yourname);//User inputs the name
-    printf("%d",yourname);
+    scanf("%s",&yourname);//User inputs the name
+    printf("%s",yourname);
 
     return 0;
 }
